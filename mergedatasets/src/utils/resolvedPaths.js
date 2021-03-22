b8fc247a497deb2b33d5424bc6f84b9bbf3b639b
Juliapp <aragaopintojuli@gmail.com>
2021-03-22 17:49:41 -0300

    merge novo dataset

--- mergedatasets/src/utils/resolvedPaths.js
+++ mergedatasets/src/utils/resolvedPaths.js
@@ -2,7 +2,7 @@ import path from "path";
 
 const rootDirectory = path.resolve("");
 
-const focosDirectory = path.resolve(rootDirectory, "..", "dataset", "focos", "FireSpotsByReferenceSatelite");
+const focosDirectory = path.resolve(rootDirectory, "..", "dataset", "focos");
 
 const metereologiaDirectory = path.resolve(rootDirectory, "..", "dataset", "meteorologia");
 
