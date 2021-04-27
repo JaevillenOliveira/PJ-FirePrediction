9b34a2ba10f972151b70b08026b519e11dff6576
Juliapp <aragaopintojuli@gmail.com>
2021-04-27 18:13:32 -0300

    :sparkles: Novas variaveis merged

--- mergedatasets/src/infra/workerCicle.js
+++ mergedatasets/src/infra/workerCicle.js
@@ -1,52 +1,44 @@
-import { workerData, parentPort } from "worker_threads";
-import jsontocsv from "json-to-csv-stream";
-import { createReadStream, createWriteStream } from 'fs'
-import { parse } from 'fast-csv'
-import focosDict from '../utils/focosDict.js'
+import { workerData, parentPort } from 'worker_threads';
+import jsontocsv from 'json-to-csv-stream';
+import { createReadStream, createWriteStream } from 'fs';
+import { parse } from 'fast-csv';
+import focosDict from '../utils/focosDict.js';
+import { rowFormat } from '../utils/rowFormat.js';
 
-const { focosPaths, dado } = workerData
-const [ currentFileName, outputFileName ] = dado
-async function run(){
-  const dirFocos = await focosDict(focosPaths)
+const { focosPaths, dado } = workerData;
+const [currentFileName, outputFileName] = dado;
+async function run() {
+  const dirFocos = await focosDict(focosPaths);
 
-  const stream = createReadStream(currentFileName)
-  const output = createWriteStream(outputFileName)
+  const stream = createReadStream(currentFileName);
+  const output = createWriteStream(outputFileName);
 
-  stream.pipe(parse({ headers: true }))
+  stream
+    .pipe(parse({ headers: true }))
     .transform((row, next) => {
-      let satelite
-      let date = row['Data Medicao']
+      let satelite;
+      let date = row['Data Medicao'];
 
-      let has_Foco = false
+      let has_Foco = false;
 
-      satelite = row['Latitude'] === '-12.557853999999999' ?  'lencois' : 'piata'
+      satelite =
+        row['Latitude'] === '-12.557853999999999' ? 'lencois' : 'piata';
 
-      if(dirFocos[satelite].includes(date)){
+      if (dirFocos[satelite].includes(date)) {
         has_Foco = true;
       }
 
-      let newRow = ``
+      let newRow = ``;
 
-      if(date > `2011-01-01` && date < `2020-12-31`){
-        let [,mes, dia] = row['Data Medicao'].split('-')
-        newRow = JSON.stringify({
-          'Data Medicao': `${dia}/${mes}`,
-          'PRECIPITACAO TOTAL; DIARIO (AUT)(mm)': row['PRECIPITACAO TOTAL; DIARIO (AUT)(mm)'],
-          'TEMPERATURA MEDIA; DIARIA (AUT)(Â°C)': row['TEMPERATURA MEDIA; DIARIA (AUT)(Â°C)'],
-          'UMIDADE RELATIVA DO AR; MEDIA DIARIA (AUT)(%)': row['UMIDADE RELATIVA DO AR; MEDIA DIARIA (AUT)(%)'],
-          has_Foco: has_Foco ? 1 : 0
-        })
+      if (date > `2011-01-01` && date < `2020-12-31`) {
+        newRow = rowFormat(has_Foco, row, outputFileName);
       }
-
-      return next(null, newRow)
-        
+      return next(null, newRow);
     })
     .pipe(jsontocsv())
-    .pipe(output)
-
-  
+    .pipe(output);
 }
 
 run().then(() => {
-  parentPort.postMessage("finished");
+  parentPort.postMessage('finished');
 });
---
+++ mergedatasets/src/utils/rowFormat.js
@@ -0,0 +1,43 @@
+export const rowFormat = (has_Foco, row, outputFileName) => {
+  let [, mes, dia] = row['Data Medicao'].split('-');
+
+  if (outputFileName.includes('_PRED')) {
+    return JSON.stringify({
+      'Data Medicao': `${dia}/${mes}`,
+      'PRECIPITACAO TOTAL; DIARIO (AUT)(mm)':
+        row['PRECIPITACAO TOTAL; DIARIO (AUT)(mm)'],
+      'TEMPERATURA MEDIA; DIARIA (AUT)(Â°C)':
+        row['TEMPERATURA MEDIA; DIARIA (AUT)(Â°C)'],
+      'UMIDADE RELATIVA DO AR; MEDIA DIARIA (AUT)(%)':
+        row['UMIDADE RELATIVA DO AR; MEDIA DIARIA (AUT)(%)'],
+      has_Foco: has_Foco ? 1 : 0,
+    });
+  } else if (outputFileName.includes('_CONVENCIONAL')) {
+    return JSON.stringify({
+      'Data Medicao': `${dia}/${mes}`,
+      'PRECIPITACAO TOTAL; DIARIO (AUT)(mm)':
+        row['PRECIPITACAO TOTAL; DIARIO (AUT)(mm)'],
+      'TEMPERATURA MEDIA; DIARIA (AUT)(Â°C)':
+        row['TEMPERATURA MEDIA; DIARIA (AUT)(Â°C)'],
+      'UMIDADE RELATIVA DO AR; MEDIA DIARIA (AUT)(%)':
+        row['UMIDADE RELATIVA DO AR; MEDIA DIARIA (AUT)(%)'],
+      'INSOLACAO TOTAL; DIARIO(h)': row['INSOLACAO TOTAL; DIARIO(h)'],
+      'VENTO; VELOCIDADE MEDIA DIARIA(m/s)':
+        row['VENTO; VELOCIDADE MEDIA DIARIA(m/s)'],
+      has_Foco: has_Foco ? 1 : 0,
+    });
+  } else if (outputFileName.includes('_COMVENTO_AUTO')) {
+    return JSON.stringify({
+      'Data Medicao': `${dia}/${mes}`,
+      'PRECIPITACAO TOTAL; DIARIO (AUT)(mm)':
+        row['PRECIPITACAO TOTAL; DIARIO (AUT)(mm)'],
+      'TEMPERATURA MEDIA; DIARIA (AUT)(Â°C)':
+        row['TEMPERATURA MEDIA; DIARIA (AUT)(Â°C)'],
+      'UMIDADE RELATIVA DO AR; MEDIA DIARIA (AUT)(%)':
+        row['UMIDADE RELATIVA DO AR; MEDIA DIARIA (AUT)(%)'],
+      'VENTO; VELOCIDADE MEDIA DIARIA (AUT)(m/s)':
+        row['VENTO; VELOCIDADE MEDIA DIARIA (AUT)(m/s)'],
+      has_Foco: has_Foco ? 1 : 0,
+    });
+  }
+};
